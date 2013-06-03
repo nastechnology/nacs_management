b2257e0797506e865e1325c858bdd316a3c9753d
marcusmyers <marcusmyers@gmail.com>
2013-06-03 08:03:31 -0400

    First test file

--- manifests/checkin.pp
+++ manifests/checkin.pp
@@ -1,37 +1,33 @@
 class nacs_management::checkin {
 
-	if ($operatingsystem == 'Darwin') or ($operatingsystem == 'Ubuntu') {
-
-            file { "/opt/NACSManage/checkin.sh":
-            	ensure  => file,
-            	owner   => "technology",
-            	group   => "staff",
-            	mode    => 0755,
-            	content => template("nacs_management/nonwindows_checkin.erb"),
-            	require => File['/opt/NACSManage'],
-            }
-
-            exec { "Checkin":
-            	command => "/bin/bash /opt/NACSManage/checkin.sh",
-            	require => File['/opt/NACSManage/checkin.sh'],
-            }
-	
+    case $operatingsystem {
+	'Darwin', 'Ubuntu': {
+	    $checkinfile = '/opt/NACSManage/checkin.sh'
+	    $command = '/bin/bash /opt/NACSManage/checkin.sh'
+	    $owner = 'technology'
+	    $group = 'staff'
+	    $template = 'nonwindows_checkin.erb'
 	}
-	else {
-	    # Assumes Windows
-	    file { "C:/NACSManage/checkin.bat":
-		ensure	=> file,
-		owner	=> 'Administrator',
-		group	=> 'Administrators',
-		mode	=> 0755,
-		content	=> template('nacs_management/windows_checkin.erb'),
-		require => File['C:/NACSManage'],
-	    }
-	
-	    exec { "Checkin":
-		command => "cmd.exe /c C:/NACSManage/checkin.bat",
-		require => File['C:/NACSManage/checkin.bat'],
-	    }	
-  	}
+	default: {
+	    $checkinfile = "C:/NACSManage/checkin.bat"
+	    $command = "cmd.exe /c C:/NACSManage/checkin.bat"
+	    $owner = 'Administrator'
+	    $group = 'Administrators'
+	    $template = 'windows_checkin.erb'
+	}
+    }
 
+
+    file { $checkinfile:
+       	ensure  => file,
+       	owner   => $owner,
+       	group   => $group,
+       	mode    => 0755,
+       	content => template("nacs_management/nonwindows_checkin.erb"),
+    }
+
+    exec { "Checkin":
+       	command => $command,
+       	require => File[$checkinfile],
+    }	
 }
