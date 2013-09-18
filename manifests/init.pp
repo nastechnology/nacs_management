3f7d053fe84a794cf59f3ded3531009dca02e24d
marcusmyers <marcusmyers@gmail.com>
2013-09-18 12:49:59 -0400

    Removed init_spec.rb

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,7 +17,7 @@
 #
 class nacs_management {
 
-    if ($operatingsystem == 'Darwin') or ($operatingsystem == 'Ubuntu'){
+  if ($operatingsystem == 'Darwin') or ($operatingsystem == 'Ubuntu'){
 	  file {"/opt/NACSManage":
         ensure  => directory,
         owner   => "technology",
@@ -25,28 +25,28 @@ class nacs_management {
         mode    => 0755,
 	  }
 
-      if($operatingsystem == 'Darwin'){
-        file {'/Library/Preferences/com.apple.loginwindow.plist':
-          ensure => present,
-	      owner => root,
-	      group => admin,
+    if($operatingsystem == 'Darwin'){
+      file {'/Library/Preferences/com.apple.loginwindow.plist':
+        ensure => present,
+	      owner  => root,
+	      group  => admin,
 	    }
 
-        exec { 'HideTechUser':
-          command => "/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add technology technologydepartment",
-        }	
+      exec { 'HideTechUser':
+        command => "/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add technology technologydepartment",
+      }	
 
-        exec {'Hide sub-500 users':
-          command => "/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow Hide500Users -bool TRUE",
-        }
+      exec {'Hide sub-500 users':
+        command => "/usr/bin/defaults write /Library/Preferences/com.apple.loginwindow Hide500Users -bool TRUE",
+      }
 
 	    property_list_key { 'LoginwindowText':
-	        ensure => present,
-	        path   => '/Library/Preferences/com.apple.loginwindow.plist',
-	        key    => 'LoginwindowText',
-	        value  => "Property of Napoleon Area City Schools, if found call 419-599-7015",
+	      ensure => present,
+        path   => '/Library/Preferences/com.apple.loginwindow.plist',
+	      key    => 'LoginwindowText',
+	      value  => "Property of Napoleon Area City Schools, if found call 419-599-7015",
 	    }
-		
+
 	    # Disable Gatekeeper in 10.8
 	    property_list_key { 'Disable Gatekeeper':
 	      ensure => present,
@@ -54,14 +54,14 @@ class nacs_management {
 	      key    => 'enabled',
 	      value  => 'no',
 	    }
-      }
-    } else {
-	  # Assumes Windows - for now
-	  file { "C:/NACSManage":
-        ensure => directory,
-        owner  => 'Everyone',
-        group  => 'Everyone',
-        mode   => 0777,
-      }
     }
+  } else {
+    # Assumes Windows - for now
+	  file { "C:/NACSManage":
+      ensure => directory,
+      owner  => 'Everyone',
+      group  => 'Everyone',
+      mode   => 0777,
+    }
+  }
 }
