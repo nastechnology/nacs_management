3819d639273e52b9403edfe858062c0c154f24d2
marcusmyers <marcusmyers@gmail.com>
2013-10-31 22:18:15 -0400

    Changed plist to txt file

--- manifests/techspecs/mac.pp
+++ manifests/techspecs/mac.pp
@@ -34,73 +34,59 @@ class nacs_management::techspecs::mac {
 
   file { '/opt/NACSManage/facts.txt':
     ensure => file,
-    owner   => 'technology',
-    group   => 'staff',
-    mode    => 0777,
+    owner  => 'technology',
+    group  => 'staff',
+    mode   => 0777,
   }
 
   exec { 'ComputerName':
     command => "/bin/echo 'ComputerName=${hostname}' >> /opt/NACSManage/facts.txt",    
   }
 
-  #property_list_key { 'ComputerName':
-  #  ensure     => present,
-  #  path       => '/Library/Preferences/org.nacs.NACSTech.techspecs.plist',
-  #  key        => 'ComputerName',
-  #  value      => $::hostname,
-  #  value_type => 'string',
-  #}
-
-  property_list_key { 'SerialNumber':
-    ensure     => present,
-    path       => '/Library/Preferences/org.nacs.NACSTech.techspecs.plist',
-    key        => 'SerialNumber',
-    value      => $::sp_serial_number,
-    value_type => 'string',
+  exec { 'SerialNumber':
+    command => "/bin/echo 'SerialNumber=${sp_serial_number}' >> /opt/NACSManage/facts.txt",
   }
 
-  property_list_key {'InventoryTag':
-    ensure      => present,
-    path        => '/Library/Preferences/org.nacs.NACSTech.techspecs.plist',
-    key         => 'InventoryTag',
-    value       => $::inventory_tag,
-    value_type  => 'string',
+  exec { 'InventoryTag':
+    command => "/bin/echo 'InventoryTag=${inventory_tag}' >> /opt/NACSManage/facts.txt",
   }
 
-  property_list_key { 'RoomNumber':
-    ensure      => present,
-    path        => '/Library/Preferences/org.nacs.NACSTech.techspecs.plist',
-    key         => 'RoomNumber',
-    value       => $::room,
-    value_type  => 'string',
+  exec { 'RoomNumber':
+    command => "/bin/echo 'RoomNumber=${room}' >> /opt/NACSManage/facts.txt",
   }
 
-  property_list_key { 'Building':
-    ensure      => present,
-    path        => '/Library/Preferences/org.nacs.NACSTech.techspecs.plist',
-    key         => 'Building',
-    value       => $::building,
-    value_type  => 'string',
+  exec { 'Building':
+    command => "/bin/echo 'Building=${Building}' >> /opt/NACSManage/facts.txt",
   }
 
-  property_list_key { 'DeviceType':
-    ensure     => present,
-    path       => '/Library/Preferences/org.nacs.NACSTech.techspecs.plist',
-    key        => 'DeviceType',
-    value      => $::mac_laptop,
-    value_type => 'string',
+  exec { 'DeviceType':
+    command => "/bin/echo 'DeviceType=${mac_laptop}' >> /opt/NACSManage/facts.txt",  
   }
 
-  property_list_key { 'SystemSpecs':
-    ensure      => present,
-    path        => '/Library/Preferences/org.nacs.NACSTech.techspecs.plist',
-    key         => 'SystemSpecs',
-    value       => [{
-      'MACAddress'      => $::macaddress,
-      'OperatingSystem' => "$::{macosx_productname} $::{macosx_productversion}",
-      'Model'           => $::sp_machine_model,
-      'Processor'       => "$::{sp_cpu_type} $::{sp_current_processor_speed}",
-      'Memory'          => $::sp_physical_memory,}],
-    value_type  => 'array',
+  file { '/opt/NACSManage/SystemSpecs.txt':
+    ensure => file,
+    owner  => 'technology',
+    group  => 'staff',
+    mode   => 0777,
+  }
+
+  exec { 'MACAddress':
+    command => "/bin/echo 'MACAddress=${macaddress}' >> /opt/NACSManage/SystemSpecs.txt",
+  }
+
+  exec { 'OperatingSystem':
+    command => "/bin/echo 'OperatingSystem=${macosx_productname} ${macosx_productversion}' >> /opt/NACSManage/SystemSpecs.txt",
+  }
+
+  exec { 'Model':
+    command => "/bin/echo 'Model=${sp_machine_model}' >> /opt/NACSManage/SystemSpecs.txt",
+  }
+  
+  exec { 'Processor':
+    command => "/bin/echo 'Processor=${sp_cpu_type} ${sp_current_processor_speed}' >> /opt/NACSManage/SystemSpecs.txt",
+  }
+
+  exec { 'Memory':
+    command => "/bin/echo 'Memory=${sp_physical_memory}' >> /opt/NACSManage/SystemSpecs.txt",
   }
 }
