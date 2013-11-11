db021fd31df6db0df20a20ab1d02e5a389257a63
Mark Myers <marcusmyers@gmail.com>
2013-11-11 14:32:43 -0500

    Added central printers

--- manifests/printers.pp
+++ manifests/printers.pp
@@ -31,8 +31,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.220/",
         description => "NHS Copier in Teacher Workroom",
         location    => "NHS Office",
-        shared	  => false,
-        ppd		  => "/Library/Printers/PPDs/Contents/Resources/KONICAMINOLTA601.gz",
+        shared	    => false,
+        ppd		      => "/Library/Printers/PPDs/Contents/Resources/KONICAMINOLTA601.gz",
 	    }
 	  }
 
@@ -42,8 +42,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.237/",
         description => "NHS Color Printer",
         location    => "Secretary Office",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-C5300DN.PPD",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-C5300DN.PPD",
       }
     }
 
@@ -53,8 +53,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.11.7/",
         description => "CBI Copier",
         location    => "Joe Goods classroom",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/en.lproj/Ricoh-Aficio_2035-pxlmono-Ricoh.ppd.gz",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/en.lproj/Ricoh-Aficio_2035-pxlmono-Ricoh.ppd.gz",
       }
     }
 
@@ -64,8 +64,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.235/",
         description => "DECA Printer",
         location    => "DECA",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-2020D.PPD",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-2020D.PPD",
       }
     }
 
@@ -75,8 +75,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.228/",
         description => "Art Printer",
         location    => "Art",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-C5350DN.PPD",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-C5350DN.PPD",
       }
     }
 
@@ -86,8 +86,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.248/",
         description => "FSCI Color printer",
         location    => "NHS 140",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-C5100DN.PPD",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-C5100DN.PPD",
       }
     }
 
@@ -97,8 +97,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.226/",
         description => "Guidance Ciopier",
         location    => "NHS Guidance",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/en.lproj/Ricoh-Aficio_MP_5001-pxlmono-Ricoh.ppd.gz",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/en.lproj/Ricoh-Aficio_MP_5001-pxlmono-Ricoh.ppd.gz",
       }
     }
 
@@ -108,8 +108,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.233",
         description => "Ricoh 7500 Copier",
         location    => "NHS 231",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/en.lproj/Ricoh-Aficio_MP_7500-pxlmono-Ricoh.ppd.gz",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/en.lproj/Ricoh-Aficio_MP_7500-pxlmono-Ricoh.ppd.gz",
       }
     }
 
@@ -119,8 +119,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.232",
         description => "Ricoh 7500 Copier 2",
         location    => "NHS 231",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/en.lproj/Ricoh-Aficio_MP_7500-pxlmono-Ricoh.ppd.gz",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/en.lproj/Ricoh-Aficio_MP_7500-pxlmono-Ricoh.ppd.gz",
       }
     }
 
@@ -130,8 +130,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.234/",
         description => "NHS Workroom 231",
         location    => "NHS 231",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-3920DN.PPD",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-3920DN.PPD",
       }
     }
 
@@ -141,8 +141,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.247",
         description => "NHS Workroom 207",
         location    => "NHS 207",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-3920DN.PPD",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-3920DN.PPD",
       }
     }
 
@@ -152,8 +152,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.254",
         description => "NHS Staff Lounge",
         location    => "NHS Staff Lounge",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-3920DN.PPD",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-3920DN.PPD",
       }
     }
 
@@ -163,8 +163,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.15.244",
         description => "NHS Library",
         location    => "NHS Library",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-3920DN.PPD",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/Kyocera FS-3920DN.PPD",
       }
     }
 
@@ -174,8 +174,8 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.11.229",
         description => "Lori Drewes",
         location    => "NHS 218",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/en.lproj/HP-LaserJet_5-pxlmono.ppd.gz",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/en.lproj/HP-LaserJet_5-pxlmono.ppd.gz",
       }
     }
 
@@ -185,8 +185,54 @@ define nacs_management::printers ($printer = $name) {
         uri         => "lpd://10.20.11.230/",
         description => "Library Aide printer",
         location    => "NHS Library",
-        shared    => false,
-        ppd     => "/Library/Printers/PPDs/Contents/Resources/en.lproj/HP-LaserJet_4000-pxlmono.ppd.gz",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/en.lproj/HP-LaserJet_4000-pxlmono.ppd.gz",
+      }
+    }
+
+    # CES Printers
+    'ces_office_copier': {
+      printer { $printer:
+        ensure      => present,
+        uri         => "lpd://10.20.35.10/",
+        description => "Central Office Copier",
+        location    => "CES Office",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/KONICAMINOLTA601.gz",
+      }
+    }
+
+    'ces_wkrm': {
+      printer { $printer:
+        ensure      => present,
+        uri         => "lpd://10.20.35.11/",
+        description => "Central Workroom Copier",
+        location    => "CES Workroom",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/KONICAMINOLTA601.gz",
+      }
+    }
+
+    'ces_upstairs_wkrm': {
+      printer { $printer:
+        ensure      => present,
+        uri         => "lpd://10.20.35.12/",
+        description => "Central Upstairs Workroom Copier",
+        location    => "CES Workroom Upstairs",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/KONICAMINOLTA601.gz",
+      }
+    }
+
+    # CDB Pritners
+    'ces_kg_colorlaser': {
+      printer { $printer:
+        ensure      => present,
+        uri         => "lpd://10.20.33.11/",
+        description => "Central Office Copier",
+        location    => "CES Office",
+        shared      => false,
+        ppd         => "/Library/Printers/PPDs/Contents/Resources/KONICAMINOLTA601.gz",
       }
     }
   }
