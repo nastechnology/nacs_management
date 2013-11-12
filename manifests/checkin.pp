25db7dbf8d7c45c397083b0f324d55164841a924
Mark Myers <marcusmyers@gmail.com>
2013-11-12 16:40:50 -0500

    Fixed so checkin only runs if not on school network

--- manifests/checkin.pp
+++ manifests/checkin.pp
@@ -17,34 +17,35 @@
 #
 class nacs_management::checkin {
 
-    case $operatingsystem {
-	'Darwin', 'Ubuntu': {
-	    $checkinfile = '/opt/NACSManage/checkin.sh'
-	    $command = '/bin/bash /opt/NACSManage/checkin.sh'
-	    $owner = 'technology'
-	    $group = 'staff'
-	    $template = 'nonwindows_checkin.erb'
-	}
-	default: {
-	    $checkinfile = "C:/NACSManage/checkin.bat"
-	    $command = "cmd.exe /c C:/NACSManage/checkin.bat"
-	    $owner = 'Administrator'
-	    $group = 'Administrators'
-	    $template = 'windows_checkin.erb'
-	}
+  case $operatingsystem {
+    'Darwin', 'Ubuntu': {
+      $checkinfile = '/opt/NACSManage/checkin.sh'
+      $command = '/bin/bash /opt/NACSManage/checkin.sh'
+      $owner = 'technology'
+      $group = 'staff'
+      $template = 'nonwindows_checkin.erb'
     }
+    default: {
+      $checkinfile = "C:/NACSManage/checkin.bat"
+      $command = "cmd.exe /c C:/NACSManage/checkin.bat"
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $template = 'windows_checkin.erb'
+    }
+  }
 
-
+  if(!$ipaddress =~ /10.20/i.to_s){
     file { $checkinfile:
-       	ensure  => file,
-       	owner   => $owner,
-       	group   => $group,
-       	mode    => 0755,
-       	content => template("nacs_management/$template"),
+      ensure  => file,
+      owner   => $owner,
+   	  group   => $group,
+   	  mode    => 0755,
+   	  content => template("nacs_management/$template"),
     }
 
     exec { "Checkin":
-       	command => $command,
-       	require => File[$checkinfile],
-    }	
+   	  command => $command,
+   	  require => File[$checkinfile],
+    }
+  }
 }
