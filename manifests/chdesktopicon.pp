da47c951e60f11077c6363e38f8e4d67bb04b3af
Mark Myers <marcusmyers@gmail.com>
2013-11-20 14:21:00 -0500

    Changed exec name to have user and filename in title

--- manifests/chdesktopicon.pp
+++ manifests/chdesktopicon.pp
@@ -25,7 +25,7 @@
 #
 define nacs_management::chdesktopicon ($user,$file = $name) {
 
-  exec { "ChangeIcon${file}":
+  exec { "ChangeIcon${user}${file}":
     command => "/opt/NACSManage/setfileicon '/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/InternetLocationGeneric.icns' '/Users/${user}/Desktop/${file}'",
     require => File["/Users/${user}/Desktop/${file}"],
   }
