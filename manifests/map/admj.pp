fed6a741513eb6710180f17fc7c920bcee5d9846
Mark Myers <marcusmyers@gmail.com>
2013-12-17 12:03:43 -0500

    Added adm j drive

---
+++ manifests/map/admj.pp
@@ -0,0 +1,41 @@
+# == Defintion: nacs_management::map::admj
+#
+# This creates an .afploc file on the users desktop
+# for admin J
+#
+# == Parameters
+#  - The $server is the server the share is on
+#  - The $username is the place for the desktop shortcut
+#
+# == Actions
+#  - Creates afploc file for network j drive
+#
+# === Examples
+#
+# nacs_management::map::admj{ '100XXXXKDrive':
+#   server => 'server.domain.local',
+#   user   => '100XXXX'
+# }
+#
+# === Authors
+#
+# Mark Myers <mark.myers@napoleonareaschools.org>
+#
+# === Copyright
+#
+# Copyright 2013 Mark Myers, unless otherwise noted.
+#
+define nacs_management::map::admj ($user) {
+  $server = "adm-fs.nasadm.local"
+  $share = "shared"
+  
+  file { "/Users/${user}/Desktop/JDrive.afploc":
+    ensure  => file,
+    content => template("nacs_management/afploc.erb"),
+  }
+
+  nacs_management::chdesktopicon { 'JDrive.afploc':
+    user => $user,
+  }
+
+}
