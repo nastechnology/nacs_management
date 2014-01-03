cbe9e9635a2269554f3270f16e65e44daa3a3862
Mark Myers <marcusmyers@gmail.com>
2014-01-03 15:11:39 -0500

    Fixed after to require

--- manifests/tmutil.pp
+++ manifests/tmutil.pp
@@ -29,66 +29,66 @@ define nacs_management::tmutil ($user = $name) {
 
   exec { "Set${userbackup}Destination":
     command => "/usr/bin/tmutil setdestination afp://${userbackup}:backup@xserve.nacswildcats.org/TimeMachines",
-    after   => Exec['EnableTmutil'],
+    require => Exec['EnableTmutil'],
   }
 
   exec { "Remove${userbackup}Desktop":
     command => "/usr/bin/tmutil addexclusion /Users/${user}/Desktop",
-    after   => Exec["Set${userbackup}Destination"],
+    require => Exec["Set${userbackup}Destination"],
   }
 
   exec { "Remove${userbackup}Downloads":
     command => "/usr/bin/tmutil addexclusion /Users/${user}/Downloads",
-    after   => Exec["Remove${userbackup}Desktop"],
+    require => Exec["Remove${userbackup}Desktop"],
   }
 
   exec { "Remove${userbackup}Dropbox":
     command => "/usr/bin/tmutil addexclusion /Users/${user}/Dropbox",
-    after   => Exec["Remove${userbackup}Downloads"],
+    require => Exec["Remove${userbackup}Downloads"],
   }
 
   exec { "Remove${userbackup}GoogleDrive":
     command => "/usr/bin/tmutil addexclusion /Users/${user}/Google\ Drive",
-    after   => Exec["Remove${userbackup}Dropbox"],
+    require => Exec["Remove${userbackup}Dropbox"],
   }
 
   exec { "Remove${userbackup}SkyDrive":
     command => "/usr/bin/tmutil addexclusion /Users/${user}/SkyDrive",
-    after   => Exec["Remove${userbackup}GoogleDrive"],
+    require => Exec["Remove${userbackup}GoogleDrive"],
   }
 
   exec { "Remove${userbackup}Library":
     command => "/usr/bin/tmutil addexclusion /Users/${user}/Library",
-    after   => Exec["Remove${userbackup}SkyDrive"],
+    require => Exec["Remove${userbackup}SkyDrive"],
   }
 
   exec { "Remove${userbackup}Movies":
     command => "/usr/bin/tmutil addexclusion /Users/${user}/Movies",
-    after   => Exec["Remove${userbackup}Library"],
+    require => Exec["Remove${userbackup}Library"],
   }
 
   exec { "Remove${userbackup}Music":
     command => "/usr/bin/tmutil addexclusion /Users/${user}/Music",
-    after   => Exec["Remove${userbackup}Movies"],
+    require => Exec["Remove${userbackup}Movies"],
   }
 
   exec { "Remove${userbackup}Pictures":
     command => "/usr/bin/tmutil addexclusion /Users/${user}/Pictures",
-    after   => Exec["Remove${userbackup}Music"],
+    require => Exec["Remove${userbackup}Music"],
   }
 
   exec { "Remove${userbackup}VirtualBoxVMs":
     command => "/usr/bin/tmutil addexclusion /Users/${user}/VirtualBox\ VMs",
-    after   => Exec["Remove${userbackup}Pictures"],
+    require => Exec["Remove${userbackup}Pictures"],
   }
 
   exec { "Remove${userbackup}Applications":
     command => "/usr/bin/tmutil addexclusion /Users/${user}/Applications",
-    after   => Exec["Remove${userbackup}VirtualBoxVMs"],
+    require => Exec["Remove${userbackup}VirtualBoxVMs"],
   }
 
   exec { "RemoveApplications":
     command => "/usr/bin/tmutil addexclusion /Applications",
-    after   => Exec["Remove${userbackup}Applications"],
+    require => Exec["Remove${userbackup}Applications"],
   }
 }
