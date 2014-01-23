915b146450b00f987ab6b0e7112a915a832e58e8
Mark Myers <marcusmyers@gmail.com>
2014-01-23 13:50:38 -0500

    Fixed ces shared to take the name as the user

--- manifests/map/cesshared.pp
+++ manifests/map/cesshared.pp
@@ -23,7 +23,8 @@
 #
 # Copyright 2013 Mark Myers, unless otherwise noted.
 #
-define nacs_management::map::cesshared ($user) {
+define nacs_management::map::cesshared {
+  $user = $name
   $server = "nas-fs.nas.local"
   $share = "ces_shared"
   
