b64daa2294e3066f1a03e6d08ac6ff6655ffe26e
Mark Myers <marcusmyers@gmail.com>
2014-07-09 12:07:34 -0400

    Fixed issue with gatekeeper

--- manifests/init.pp
+++ manifests/init.pp
@@ -79,6 +79,7 @@ class nacs_management {
         file { '/opt/NACSManage/new.txt':
           ensure => absent,
         }
+
       } else {
         exec { 'HideTechUser':
           command => "defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add technology technologydepartment",
@@ -99,7 +100,7 @@ class nacs_management {
       # Disable Gatekeeper in 10.8
       exec { 'Disable Gatekeeper':
         command => '/usr/sbin/spctl --master-disable',
-        unless  => "/usr/sbin/spctl --status | if [ `grep -c 'disabled'` == 1 ]; then echo 1; fi",
+        unless  => "/usr/sbin/spctl --status | if [ `grep -c 'disabled'` == 1 ]; then exit 0; fi",
       }
 
     }
